f179bd1778208f8c3dd1fb914d3f650edacb651e
elwhy-creator <wahyucinory@gmail.com>
2025-08-16 08:56:13 +0700

    Update songs.js
    Signed-off-by: elwhy-creator <wahyucinory@gmail.com>

--- static/js/songs.js
+++ static/js/songs.js
@@ -44,22 +44,3 @@ fetch('static/songs.json')
     });
   });
 
-    var songs = document.getElementsByClassName('song');
-
-    for (var i = 0; i < songs.length; i++) {
-        songs[i].addEventListener('ended', function() {
-            var currentSongIndex = -1;
-            for (var j = 0; j < songs.length; j++) {
-                if (songs[j] === this) {
-                    currentSongIndex = j;
-                    break;
-                }
-            }
-            var nextSongIndex = currentSongIndex + 1;
-            if (nextSongIndex < songs.length) {
-                songs[nextSongIndex].play();
-            } else {
-                songs[0].play();
-            }
-        });
-    }
